6736270e3562bb59f4897fe7a6be6de1e77bb3eb
Demidov, Yevgeny <Yevgeny.Demidov@nedra.digital>
2023-07-05 23:40:52 +0300

    add steps

--- src/test/java/com/demoqa/pages/RegistrationPage.java
+++ src/test/java/com/demoqa/pages/RegistrationPage.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.demoqa.pages.components.CalendarForm;
 import com.demoqa.tests.TestBase;
+import io.qameta.allure.Step;
 
 import java.io.File;
 
@@ -31,6 +32,7 @@ public class RegistrationPage {
 			submitButton = $("#submit"),
 			titleRegForm = $(byText("Student Registration Form"));
 
+	@Step("Открываем страницу")
 	public RegistrationPage openPage() {
 		Selenide.open("/automation-practice-form");
 		titleRegForm.shouldBe(visible);
@@ -38,75 +40,86 @@ public class RegistrationPage {
 		return this;
 	}
 
-
+	@Step("Заполняем поле firstName значением {firstName}")
 	public RegistrationPage setFirstName(String firstName) {
 		firstNameInput.setValue(firstName);
 		return this;
 	}
 
+	@Step("Заполняем поле lastName значением {lastName}")
 	public RegistrationPage setLastName(String lastName) {
 		lastNameInput.setValue(lastName);
 		return this;
 	}
 
+	@Step("Заполняем поле email значением {email}")
 	public RegistrationPage setEmail(String email) {
 		userEmailInput.setValue(email);
 		return this;
 	}
 
+	@Step("Устанавливаем пол {gender}")
 	public RegistrationPage setGender(String gender) {
 		genderCheckBox.
-				$(withText(gender.substring(0, 1).toUpperCase()+ gender.substring(1))).
+				$(withText(gender.substring(0, 1).toUpperCase() + gender.substring(1))).
 				click();
 		return this;
 	}
 
+	@Step("Заполняем поле mobile значением {mobile}")
 	public RegistrationPage setMobile(String mobile) {
 		mobileInput.setValue(mobile);
 		return this;
 	}
 
-
+	@Step("Заполняем поле subjects значением {subjects}")
 	public RegistrationPage setSubjects(String subjects) {
 		subjectsInput.setValue(subjects).pressEnter();
 		return this;
 	}
 
-	public RegistrationPage setHobbies(String hobbie) {
-		hobbies.$(byText(hobbie)).click();
+	@Step("Заполняем поле hobby значением {hobby}")
+	public RegistrationPage setHobbies(String hobby) {
+		hobbies.$(byText(hobby)).click();
 		return this;
 	}
 
+	@Step("Загружаем фото")
 	public RegistrationPage setPicture(String pictureName, String pathPicture) {
 		picture.uploadFile(new File(pathPicture + pictureName));
 		return this;
 	}
 
+	@Step("Устанавливаем CurrentAddress в значение {address}")
 	public RegistrationPage setCurrentAddress(String address) {
 		currentAddress.setValue(address);
 		return this;
 	}
 
+	@Step("Устанавливаем state в значение {state}")
 	public RegistrationPage setStateInput(String state) {
 		stateInput.click();
 		stateInput.$(byText(state)).click();
 		return this;
 	}
 
+	@Step("Устанавливаем city в значение {city}")
 	public RegistrationPage setCityInput(String city) {
-		if(city!=null){
+		if (city != null) {
 			cityInput.click();
 			cityInput.$(byText(city)).click();
 		}
 		return this;
 	}
 
+	@Step("Указываем дату рождения {day} {month} {year}")
 	public RegistrationPage setBirthDay(String day, String month, String year) {
 		dateOfBirth.click();
 		calendarForm.setDate(day, month, year);
 		return this;
 	}
 
+	@Step("Нажать на кнопку Submit")
 	public RegistrationPage clickSubmitButton() {
 		submitButton.click();
 		return this;
--- src/test/java/com/demoqa/pages/components/ResultsModalForm.java
+++ src/test/java/com/demoqa/pages/components/ResultsModalForm.java
@@ -1,6 +1,7 @@
 package com.demoqa.pages.components;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -13,16 +14,19 @@ public class ResultsModalForm {
 			submittingForm = $(".table-responsive"),
 			title = $("#example-modal-sizes-title-lg");
 
+	@Step("Проверить заголовок модального окна")
 	public ResultsModalForm checkTitle() {
 		title.shouldBe(visible);
 		return this;
 	}
 
+	@Step("Проверить лейбл")
 	public ResultsModalForm checkLabel() {
 		submittingForm.$(byText("Label")).sibling(0).shouldHave(text("Values"));
 		return this;
 	}
 
+	@Step("Проверить, что полное имя {firstName} {lastName}")
 	public ResultsModalForm checkName(String firstName, String lastName) {
 		submittingForm.$(byText("Student Name")).
 				sibling(0).shouldHave(text(firstName + " " + lastName));
@@ -30,22 +34,26 @@ public class ResultsModalForm {
 
 	}
 
+	@Step("Проверить, что email соответствует {email}")
 	public ResultsModalForm checkEmail(String email) {
 		submittingForm.$(byText("Student Email")).sibling(0).shouldHave(text((email)));
 		return this;
 	}
 
+	@Step("Проверить, что gender соответствует {gender}")
 	public ResultsModalForm checkGender(String gender) {
 		submittingForm.$(byText("Gender")).sibling(0).shouldHave(text(gender));
 		return this;
 	}
 
+	@Step("Проверить, что mobile соответствует {mobile}")
 	public ResultsModalForm checkMobile(String mobile) {
 		submittingForm.$(byText("Mobile")).sibling(0).shouldHave(text(mobile));
 		return this;
 
 	}
 
+	@Step("Проверить, что дата рождения соответствует {day} {month} {year}")
 	public ResultsModalForm checkBirth(String day, String month, String year) {
 		submittingForm.$(byText("Date of Birth")).
 				sibling(0).
@@ -53,26 +61,31 @@ public class ResultsModalForm {
 		return this;
 	}
 
+	@Step("Проверить, что subjects соответствует {subjects}")
 	public ResultsModalForm checkSubjects(String subjects) {
 		submittingForm.$(byText("Subjects")).sibling(0).shouldHave(text(subjects));
 		return this;
 	}
 
+	@Step("Проверить, что hobbies соответствует {hobbies}")
 	public ResultsModalForm checkHobbies(String hobbies) {
 		submittingForm.$(byText("Hobbies")).sibling(0).shouldHave(text(hobbies));
 		return this;
 	}
 
+	@Step("Проверить, что путь к picture соответствует {picture}")
 	public ResultsModalForm checkPicture(String picture) {
 		submittingForm.$(byText("Picture")).sibling(0).shouldHave(text(picture));
 		return this;
 	}
 
+	@Step("Проверить, что address соответствует {address}")
 	public ResultsModalForm checkAddress(String address) {
 		submittingForm.$(byText("Address")).sibling(0).shouldHave(text(address));
 		return this;
 	}
 
+	@Step("Проверить, что state  и city соответствует {state} {city}")
 	public ResultsModalForm checkStateCity(String state, String city) {
 		submittingForm.$(byText("State and City")).
 				sibling(0).shouldHave(text(state + " " + city));
