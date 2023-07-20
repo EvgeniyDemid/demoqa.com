ca6cb9c70f1a9e4d13431801fc5204559e3c8173
Demidov, Yevgeny <Yevgeny.Demidov@nedra.digital>
2023-07-20 12:56:27 +0300

    edit WebConfig

--- src/test/java/com/demoqa/config/WebConfig.java
+++ src/test/java/com/demoqa/config/WebConfig.java
@@ -1,4 +1,5 @@
 package com.demoqa.config;
+
 import org.aeonbits.owner.Config;
 
 @Config.LoadPolicy(Config.LoadType.MERGE)
@@ -6,22 +7,27 @@ import org.aeonbits.owner.Config;
 		"system:properties",
 		"classpath:${env}.properties"
 })
-public interface WebConfig extends Config{
+public interface WebConfig extends Config {
 	@Key("browser")
 	@DefaultValue("chrome")
 	String browser();
+
 	@Key("browserVersion")
 	@DefaultValue("100.0")
 	String browserVersion();
+
 	@Key("browserSize")
 	@DefaultValue("1920x1080")
 	String browserSize();
+
 	@Key("baseUrl")
 	@DefaultValue("https://demoqa.com")
 	String baseUrl();
+
 	@Key("isRemote")
 	@DefaultValue("true")
 	boolean isRemote();
+
 	@Key("remoteUrl")
 	@DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
 	String remoteUrl();
